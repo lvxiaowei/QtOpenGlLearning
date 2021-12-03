42b86568603b7eb1493d511e7abc643530febbc9
lvxiaowei <x.lv@staubli.com>
2021-12-03 13:33:46 +0800

    最简单的矩形显示提交

---
+++ glwidget.cpp
@@ -0,0 +1,135 @@
+﻿#include "glwidget.h"
+#include <QOpenGLShaderProgram>
+#include <QDebug>
+
+class GLWidgetData : public QSharedData
+{
+public:
+
+};
+
+GLWidget::GLWidget(QWidget *parent) : QOpenGLWidget(parent)
+  , data(new GLWidgetData)
+{
+
+}
+
+GLWidget::GLWidget(const GLWidget &rhs) : data(rhs.data)
+{
+
+}
+
+GLWidget &GLWidget::operator=(const GLWidget &rhs)
+{
+    if (this != &rhs)
+        data.operator=(rhs.data);
+    return *this;
+}
+
+GLWidget::~GLWidget()
+{
+    cleanup();
+}
+
+void GLWidget::cleanup()
+{
+    if (m_program == nullptr)
+        return;
+    makeCurrent();
+    m_logoVbo.destroy();
+    delete m_program;
+    m_program = 0;
+    doneCurrent();
+}
+
+static const char *vertexShaderSource =
+        "#version 330\n"
+        "in vec3 posVertex;\n"
+        "void main() {\n"
+        "   gl_Position = vec4(posVertex, 1.0);\n"
+        "}\n";
+
+static const char *fragmentShaderSource =
+        " #version 330\n"
+        " out vec4 fragColor;"
+        " void main() {"
+        "   fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+        " }";
+
+GLfloat m_data[] = {
+    // 第一个三角形
+    0.5f, 0.5f, 0.0f,   // 右上角
+    0.5f, -0.5f, 0.0f,  // 右下角
+    -0.5f, 0.5f, 0.0f,  // 左上角
+    // 第二个三角形
+    0.5f, -0.5f, 0.0f,  // 右下角
+    -0.5f, -0.5f, 0.0f, // 左下角
+    -0.5f, 0.5f, 0.0f   // 左上角
+};
+
+void GLWidget::initializeGL()
+{
+    connect(context(), &QOpenGLContext::aboutToBeDestroyed, this, &GLWidget::cleanup);
+
+    initializeOpenGLFunctions();
+    glClearColor(0, 0, 0, 1);
+
+    m_program = new QOpenGLShaderProgram;
+    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex,  vertexShaderSource);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    m_program->bindAttributeLocation("posVertex", 0);
+    //    m_program->bindAttributeLocation("normal", 1);
+    if(m_program->link())
+    {
+        qDebug("link success");
+    }
+    else
+    {
+        qDebug("link failed");
+    }
+
+    m_vao.create();
+    QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
+
+    m_logoVbo.create();
+    m_logoVbo.bind();
+    m_logoVbo.allocate(m_data, 2*3*3 * sizeof(GLfloat));
+
+
+    m_logoVbo.bind();
+    QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
+    f->glEnableVertexAttribArray(0);
+    f->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+    m_logoVbo.release();
+
+    //    m_camera.setToIdentity();
+    //    m_camera.translate(0, 0, -1);
+
+    //         m_program->setUniformValue(m_lightPosLoc, QVector3D(0, 0, 70));
+
+    m_program->release();
+}
+
+void GLWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+    // 设置顺时针方向 CW : Clock Wind 顺时针方向
+    // 默认是 GL_CCW : Counter Clock Wind 逆时针方向
+    glFrontFace(GL_CW);
+
+    QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
+    m_program->bind();
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    m_program->release();
+}
+
+void GLWidget::resizeGL(int w, int h)
+{
+    m_proj.setToIdentity();
+    m_proj.perspective(45.0f, GLfloat(w) / h, 0.01f, 100.0f);
+}
---
+++ glwidget.h
@@ -0,0 +1,44 @@
+﻿#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QSharedDataPointer>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+#include <QMatrix4x4>
+
+class GLWidgetData;
+
+QT_FORWARD_DECLARE_CLASS(QOpenGLShaderProgram)
+
+class GLWidget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+public:
+    GLWidget(QWidget *parent = 0);
+    GLWidget(const GLWidget &);
+    GLWidget &operator=(const GLWidget &);
+    ~GLWidget();
+
+public slots:
+    void cleanup();
+
+protected:
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int width, int height) override;
+
+private:
+    QSharedDataPointer<GLWidgetData> data;
+
+    QOpenGLVertexArrayObject m_vao;
+    QOpenGLBuffer m_logoVbo;
+    QOpenGLShaderProgram *m_program;
+
+    QMatrix4x4 m_camera;
+    QMatrix4x4 m_proj;
+    QMatrix4x4 m_world;
+};
+
+#endif // GLWIDGET_H
