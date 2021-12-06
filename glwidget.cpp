0b4d0a1458fc9183bd431d5cbb8b095a8f75ba39
lvxiaowei <x.lv@staubli.com>
2021-12-06 14:23:37 +0800

    绘制10个矩形

--- glwidget.cpp
+++ glwidget.cpp
@@ -109,6 +109,19 @@ GLfloat vertices[] = {
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 GLenum indices[] = {
     0, 1, 3, // 第一个三角形
     1, 2, 3  // 第二个三角形
@@ -117,18 +130,18 @@ GLenum indices[] = {
 void GLWidget::initializeGL()
 {
     QTimer *timer = new QTimer(this);
-    timer->setInterval(50);
+    timer->setInterval(10);
     connect(timer, &QTimer::timeout, [=]{
         t +=10;
         t = t%360;
-        update();
+        //update();
     });
     timer->start();
 
     connect(context(), &QOpenGLContext::aboutToBeDestroyed, this, &GLWidget::cleanup);
 
     initializeOpenGLFunctions();
-    glClearColor(0, 0, 0, 1);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
     m_program = new QOpenGLShaderProgram;
     m_program->addShaderFromSourceCode(QOpenGLShader::Vertex,  vertexShaderSource);
@@ -198,16 +211,11 @@ void GLWidget::paintGL()
     m_program->setUniformValue("texture1", 0);
     m_program->setUniformValue("texture2", 1);
 
-
-    glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
     glm::mat4 view          = glm::mat4(1.0f);
     glm::mat4 projection    = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians((float)t), glm::vec3(0.5f, 1.0f, 0.0f));
+
     view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
     projection = glm::perspective(glm::radians(45.0f), GLfloat(width()) / height(), 0.1f, 100.0f);
-
-    unsigned int transformLoc_i = m_program->uniformLocation("model");
-    glUniformMatrix4fv(transformLoc_i, 1, GL_FALSE, glm::value_ptr(model));
 
     unsigned int view_i = m_program->uniformLocation("view");
     glUniformMatrix4fv(view_i, 1, GL_FALSE, glm::value_ptr(view));
@@ -215,7 +223,20 @@ void GLWidget::paintGL()
     unsigned int projection_i = m_program->uniformLocation("projection");
     glUniformMatrix4fv(projection_i, 1, GL_FALSE, glm::value_ptr(projection));
 
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    for(int i=0; i < 10; ++i)
+    {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+
+        float angle = 20.0f * i;
+        model = glm::rotate(model,  glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+        unsigned int transformLoc_i = m_program->uniformLocation("model");
+        glUniformMatrix4fv(transformLoc_i, 1, GL_FALSE, glm::value_ptr(model));
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
     //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     m_program->release();
